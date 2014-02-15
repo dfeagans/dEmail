343633bbbe2e89823faf21e08ed89daa8f32a306
dfeagans <dane.feagans@gmail.com>
2014-02-15 23:33:01 +0000

    My template wasn't working because I incorrectly tab'd a <br> tag, so Jade ignored everything after that. dEmail is now printing the results to console for quick viewing as well.

--- dEmail.js
+++ dEmail.js
@@ -6,7 +6,8 @@ var fs = require('fs');
 var html = jade.renderFile('simpleTemplate.jade', { pretty : true});
 
 fs.writeFile('index.html', html, function(err){
-    console.log("Compiled index.html");
+//    console.log("Compiled index.html");
+    console.log(html);
     if (err) {
 	console.log(err);
     }
