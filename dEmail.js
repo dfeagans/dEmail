66e8e21e1789ba04564628cf9eb4817310ccc28f
dfeagans <dane.feagans@gmail.com>
2014-02-15 22:47:25 +0000

    Implemented the 'pretty' formatting correctly in Jade.

--- dEmail.js
+++ dEmail.js
@@ -3,7 +3,7 @@
 var jade = require('jade');
 var fs = require('fs');
 
-var html = jade.renderFile('simpleTemplate.jade', 'pretty');
+var html = jade.renderFile('simpleTemplate.jade', { pretty : true});
 
 fs.writeFile('index.html', html, function(err){
     console.log("Compiled index.html");
