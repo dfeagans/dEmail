cbfb59b20954d18c3922a3a7e38a055c6cbc4ea3
dfeagans <dane.feagans@gmail.com>
2014-03-01 12:56:15 +0000

    Instead of using grunt watch to create the index.html, I just make a keybinding in emacs to compile it.

--- dEmail.js
+++ dEmail.js
@@ -9,9 +9,33 @@ var leaderboard = JSON.parse(leaderboardStr);
 var html = jade.renderFile('simpleTemplate.jade', { pretty : true, leaderboard: leaderboard });
 
 fs.writeFile('index.html', html, function(err){
-//    console.log(html);
+    console.log("index.html created at " + getDateTime());
     if (err) {
 	console.log(err);
     }
 });
 
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return hour + ":" + min + ":" + sec;
+
+}
