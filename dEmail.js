88b277e137a98a8c2464c23f6bc469a79404281f
dfeagans <dane.feagans@gmail.com>
2014-03-10 18:40:01 +0000

    Removed getDateTime function and used util.log

--- dEmail.js
+++ dEmail.js
@@ -2,6 +2,7 @@
 
 var jade = require('jade');
 var fs = require('fs');
+var util = require('util');
 
 var leaderboardStr = fs.readFileSync('leaderboard.json', 'utf8');
 var leaderboard = JSON.parse(leaderboardStr);
@@ -9,33 +10,8 @@ var leaderboard = JSON.parse(leaderboardStr);
 var html = jade.renderFile('simpleTemplate.jade', { pretty : true, leaderboard: leaderboard });
 
 fs.writeFile('index.html', html, function(err){
-    console.log("index.html created at " + getDateTime());
+    util.log("index.html created");
     if (err) {
 	console.log(err);
     }
 });
-
-function getDateTime() {
-
-    var date = new Date();
-
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-
-    var year = date.getFullYear();
-
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    return hour + ":" + min + ":" + sec;
-
-}
