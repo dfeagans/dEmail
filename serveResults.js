b227cf50a3be81708fad9da680ddc414f7ae9b6a
dfeagans <dane.feagans@gmail.com>
2014-03-16 19:55:18 +0000

    Adding option to specify the raceID. If you don't, then it just finds the most recent one. I need to work out the logic and how it will mesh with node async though.

--- serveResults.js
+++ serveResults.js
@@ -8,15 +8,25 @@ var sendResults = require('./dEmail.js');
 http.createServer(function (request, response) {
     var target = url.parse(request.url, true);
     var emailName = target.query.email;
+    var raceID = target.query.raceID;
     var displayMessage = undefined;
     
     if (target.pathname === '/RequestResults') {
 	var emailAddress = approvedEmailList(emailName);
 	if (emailAddress){
+	    if (raceID){
+		http.get('http://www.nascar.com/leaderboard/Series_1/2014/' + raceID  + '/1/leaderboard.json', function(){
+		    
+
+		});
+	    }
+	    else {
+		getMostRecentLeaderboard;
+	    }
 	    sendResults(emailAddress);
-	    displayMessage = "RESULTS SENT TO " + emailAddress;
+	    displayMessage = "Results sent to " + emailAddress;
 	} else {
-	    displayMessage = "NOT APPROVED EMAIL";
+	    displayMessage = "Email Not Approved";
 	}
     } else {
 	displayMessage = 'Please use dev.okdane.com/RequestResults?email=firstPartOfEmail syntax';
@@ -27,8 +37,9 @@ http.createServer(function (request, response) {
 }).listen(8080);
 
 
-
-
+function getMostRecentLeaderboard(){
+    return "TEST"
+}
 
 function approvedEmailList(emailLocalPart){
 
