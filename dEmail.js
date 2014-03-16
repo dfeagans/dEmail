c5719995603544c9b1acc0d733835c081271de35
dfeagans <dane.feagans@gmail.com>
2014-03-16 11:18:07 +0000

    Instead of just creating an index.html to host, dEmail now sends the email out. Still need to add integrated email attachments.

--- dEmail.js
+++ dEmail.js
@@ -1,13 +1,49 @@
 #!/usr/bin/env node
+"use strict";
 
-var jade = require('jade');
 var fs = require('fs');
-var util = require('util');
+var jade = require('jade');
+var nodemailer = require('nodemailer');
+
+var transport = nodemailer.createTransport("SMTP", {
+        service: 'Gmail',
+        auth: {
+            user: "dEmail500@gmail.com",
+            pass: ""
+        }
+    });
+
+console.log('SMTP Configured');
 
 var leaderboardStr = fs.readFileSync('leaderboard.json', 'utf8');
 var leaderboard = JSON.parse(leaderboardStr);
 
-var html = jade.renderFile('simpleTemplate.jade', { pretty : true, leaderboard: leaderboard });
+var htmlContent = jade.renderFile('simpleTemplate.jade', { pretty : true, leaderboard: leaderboard });
+
+var message = {
+    from: 'dEmail500@gmail.com',
+    to: 'dEmail500@gmail.com',
+    subject: 'Team Penske ' + leaderboard.TrackName + ' Cup Series Practice Update',
+    html: htmlContent,
+    attachments:[
+	
+    ]
+};
+
+console.log('Sending Mail');
+
+transport.sendMail(message, function(error){
+    if(error){
+        console.log('Error occured');
+        console.log(error.message);
+        return;
+    }
+    console.log('Message sent successfully!');
+    // if you don't want to use this transport object anymore, uncomment following line
+    transport.close(); // close the connection pool
+});
+
+/**********
 
 fs.writeFile('index.html', html, function(err){
     util.log("index.html created");
@@ -15,3 +51,5 @@ fs.writeFile('index.html', html, function(err){
 	console.log(err);
     }
 });
+
+***********/
