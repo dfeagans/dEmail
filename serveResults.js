25d0b6376c68badc4e936d4a9fbf4e3a1ffd329d
dfeagans <dane.feagans@gmail.com>
2014-03-19 21:12:16 +0000

    Added functionality: If you include a raceID query parameter it will grab the data for that specific race. If you leave the raceID parameter out, I plan on grabbing the most recent race data. I need to add some error checking to the function though (in-case the raceID provided is incorrect).

--- serveResults.js
+++ serveResults.js
@@ -3,13 +3,13 @@
 The config.json file separates the email address username and password that the emails will be sent from. It also contains a list of approved emails to send to. Each approved email can be pulled out using an identifier. It must be a JSON formatted as follows:
 {
     "email": {
-        "user": "emailAddresstoSendFrom@email.com",
+	"user": "emailAddresstoSendFrom@email.com",
 	"password": "unbelievablyStrongPasswordForYourAccount"
     },
     "approvedEmails": {
-        "identifier1": "fullEmail@1.com"
-        "identifier2": "fullEmail@2.com"
-        "identifier3": "fullEmail@3.com"
+	"identifier1": "fullEmail@1.com"
+	"identifier2": "fullEmail@2.com"
+	"identifier3": "fullEmail@3.com"
     }
 }
 
@@ -17,6 +17,7 @@ The config.json file separates the email address username and password that the 
 
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 var sendResults = require('./dEmail.js');
 var config = require('./config.json');
 
@@ -24,34 +25,57 @@ http.createServer(function (request, response) {
     var target = url.parse(request.url, true);
     var emailName = target.query.email;
     var raceID = target.query.raceID;
-    var displayMessage = undefined;
-    
+    var displayMessage;
+
     if (target.pathname === '/RequestResults') {
 	var emailAddress = config.approvedEmails[emailName];
-	if (emailAddress){
-	    if (raceID){
-		console.log('Got Leaderboard for RaceID: ' + raceID)
-//		http.get('http://www.nascar.com/leaderboard/Series_1/2014/' + raceID  + '/1/leaderboard.json', function(){
-
-//		});
-
+	if (emailAddress) {
+	    if (raceID) {
+		getLeaderboard(raceID, function(err){
+		    if (err) {
+			displayMessage=err;
+		    }
+		    else {
+			sendResults(emailAddress);
+		    }
+		});
 	    }
 	    else {
-		getCurrentLeaderboard();
+		getCurrentLeaderboard(function(err){
+		    if (err) {
+			displayMessage=err;
+		    }
+		    else {
+			sendResults(emailAddress);
+		    }
+		});
 	    }
-	    sendResults(emailAddress);
 	    displayMessage = "Results sent to " + emailAddress;
 	} else {
 	    displayMessage = "Email Not Approved";
 	}
     } else {
-	displayMessage = 'Please use dev.okdane.com/RequestResults?email=firstPartOfEmail syntax';
+	displayMessage = 'Please use dev.okdane.com/RequestResults?email=identifier syntax';
     }
 
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end(displayMessage);
 }).listen(8080);
 
+function getLeaderboard(raceRequest, callback){
+    var leaderboardJSON = '';
+    http.get('http://www.nascar.com/leaderboard/Series_1/2014/' + raceRequest  + '/1/leaderboard.json', function(response){
+	response.on('data', function(chunk){
+	    leaderboardJSON += chunk;
+	});
+	response.on('end', function(){
+	    fs.writeFile('leaderboard.json', leaderboardJSON, function (err){
+		console.log('Got Leaderboard for RaceID: ' + raceRequest);
+		return callback(err);
+	    });
+	});
+    });
+}
 
 function getCurrentLeaderboard(){
     console.log("Grabbed Current Leaderboard");
